31eb38ad49063dce35e39b476f76d475736b44a3
点点杯子 <tnzhuce@qq.com>
2015-08-27 16:03:17 +0800

    遮罩时禁滚动

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -3,9 +3,9 @@ var win_width;
 (function ($) {
 
     $(window).load(function () {
-        $('.noScroll').bind('touchmove',noMove);
         $("#status").fadeOut(); // will first fade out the loading animation
         $("#preloader").delay(400).fadeOut("slow"); // will fade out the white DIV that covers the website.
+        $('.noScroll').bind('touchmove',noMove);
     });
     function noMove(e){
         e.preventDefault();
